264fc09c23a50c74b7fd0ed553d5bc101d4782fd
tahgio <99019446+tahgio@users.noreply.github.com>
2022-03-14 11:04:36 +0000

    Update map-the-debris.js

--- map-the-debris.js
+++ map-the-debris.js
@@ -1,3 +1,8 @@
+//Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+//The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+//The values should be rounded to the nearest whole number. The body being orbited is Earth.
+//The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
 const GM = 398600.4418;
 const earthRadius = 6367.4447;
 
