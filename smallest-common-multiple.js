c759095e138f4763b0fe61b016b4c47cd74d3a22
tahgio <99019446+tahgio@users.noreply.github.com>
2022-03-14 11:07:12 +0000

    Update smallest-common-multiple.js

--- smallest-common-multiple.js
+++ smallest-common-multiple.js
@@ -1,3 +1,8 @@
+//Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+//The range will be an array of two numbers that will not necessarily be in numerical order.
+//For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+
+
 function smallestCommons(arr) {
   const [min, max] = arr.sort((a,b)=>a-b);
   const range = Array(max - min + 1)
